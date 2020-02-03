12b78bbf50df6097f2ea4941b3478e7440c6296a
Erick <erickescribaa@gmail.com>
2020-02-03 12:33:02 -0500

    (#2) feat: implement nine to fourteen new slides

---
+++ src/components/Nineth.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const EighthSlide = props => {
+  return (
+    <div>
+      <h1>Getting Started</h1>
+      {props.children}
+    </div>
+  );
+};
+
+export default EighthSlide;

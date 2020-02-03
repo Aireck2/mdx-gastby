e0ff200139c169927218bcaf285aa568c4c7e3d7
Erick <erickescribaa@gmail.com>
2020-02-03 17:39:15 -0500

    (#3) feat: implement animation 1rst slide

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -3,17 +3,20 @@ import "./style.css";
 const FirstSlide = props => {
   return (
     <div>
-      <div className="triangulo_top_left"></div>
-      <div className="triangulo_bottom_right"></div>
-      <p className="bold">Introduction to </p>
-      <div className="flex-center mb">
+      <div className="triangulo_top_left"> </div>
+      <div className="triangulo_bottom_right"> </div>
+      <p className="bold"> Introduction to </p>
+      <div id="scaleUp" className="flex-center mb">
         <img
-          style={{ width: 400, height: "100%" }}
+          style={{
+            width: 400,
+            height: "100%"
+          }}
           src="https://user-images.githubusercontent.com/841294/53402609-b97a2180-39ba-11e9-8100-812bab86357c.png"
         />
-        <h2>Client</h2>
+        <h2> Client </h2>
       </div>
-      <p>by</p>
+      <p> by </p>
       <p className="bold">Erick Escriba Arango</p>
       {props.children}
     </div>

fbb836980d4c24384e3828497652e50138977d62
Erick <erickescribaa@gmail.com>
2020-02-03 17:41:48 -0500

    (#3) chore: implement 12th slide

--- src/components/Twelfth.jsx
+++ src/components/Twelfth.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./style.css";
 
 const EighthSlide = props => {
-  return <div>{props.children}</div>;
+  return (
+    <div>
+      <h2>useSubscription</h2>
+    </div>
+  );
 };
 
 export default EighthSlide;

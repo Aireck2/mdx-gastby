a9e928d513414eac85b88dd4350b8d3b3cce47d5
Erick <erickescribaa@gmail.com>
2020-02-03 17:40:32 -0500

    (#3) chore: implement 13th last slide

--- src/components/Thirteenth.jsx
+++ src/components/Thirteenth.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./style.css";
 
 const EighthSlide = props => {
-  return <div>{props.children}</div>;
+  return (
+    <div>
+      <div className="wrapper">
+        <ul className="bg-bubbles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <h2 className="thanks">Thanks</h2>
+    </div>
+  );
 };
 
 export default EighthSlide;
